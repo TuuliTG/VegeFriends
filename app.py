c627c35de2ef0cc20d4640671c3699c11ddd2816
Tuuli TG <tuuli.k.toivanen@gmail.com>
2021-05-11 21:46:36 +0300

    Käyttäjän luominen ja kirjautuminen onnistuu

--- app.py
+++ app.py
@@ -1,12 +1,53 @@
 from flask import Flask
-from flask import redirect, render_template
+from flask import redirect, render_template, request, session
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import check_password_hash, generate_password_hash
+
 from os import getenv
 
 app = Flask(__name__)
+app.secret_key = getenv("SECRET_KEY")
 app.config["SQLALCHEMY_DATABASE_URI"] = getenv("DATABASE_URL")
 db = SQLAlchemy(app)
 
 @app.route("/")
 def index():
     return render_template("index.html")
+
+@app.route("/login", methods=["POST"])
+def login():
+    username = request.form["username"]
+    password = request.form["password"]
+
+    sql = "SELECT password FROM users WHERE username=:username"
+    result = db.session.execute(sql, {"username":username})
+    user = result.fetchone()
+    if user == None:
+        print("Väärä käyttäjätunnus")
+    else:
+        hash_value = user[0]
+        if check_password_hash(hash_value,password):
+            session["username"] = username
+        else:
+            print("väärä salasana")
+    return redirect("/")
+
+@app.route("/signup")
+def signup():
+    return render_template("/signup.html")
+
+
+@app.route("/logout")
+def logout():
+    del session["username"]
+    return redirect("/")
+
+@app.route("/newuser", methods=["POST"])
+def newUser():
+    username = request.form["username"]
+    password = request.form["password"]
+    hash_value = generate_password_hash(password)
+    sql = "INSERT INTO users (username, password) VALUES (:username,:password)"
+    db.session.execute(sql, {"username":username,"password":hash_value})
+    db.session.commit()
+    return redirect("/")
