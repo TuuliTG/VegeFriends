b51bf1aa18d89194dc09f413c998c6eb80997ae4
Tuuli TG <tuuli.k.toivanen@gmail.com>
2021-05-12 13:53:40 +0300

    aloitettu reseptin lisäys -toiminnot

--- schema.sql
+++ schema.sql
@@ -1,1 +1,3 @@
 CREATE TABLE users (id SERIAL PRIMARY KEY, username TEXT UNIQUE, password TEXT);
+
+CREATE TABLE recipes (id SERIAL PRIMARY KEY, title TEXT, description TEXT, user_id INTEGER REFERENCES users);
